c651d0514877730b3c39fb4b5bb68affd0cdc7c7
Harsh R Jain <rjainharsh@gmail.com>
2021-09-01 17:47:58 +0530

    ADDED: ability to create linked hash tables using arrayList

--- src/com/bridgelabz/hashmap/HashMapTest.java
+++ src/com/bridgelabz/hashmap/HashMapTest.java
@@ -4,17 +4,18 @@ public class HashMapTest {
 
 	public static void main(String[] args) {
 		
-		String sentence = "To be or not to be";
-		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashmap = new MyLinkedHashMap<>();
 		String[] words = sentence.toLowerCase().split(" ");
 		for(String word : words) {
-			Integer value = myHashMap.get(word);
+			Integer value = myLinkedHashmap.get(word);
 			if(value==null) value=1;
 			else value = value+1;
-			myHashMap.add(word, value);
+			myLinkedHashmap.add(word, value);
 		}
-		int frequency = myHashMap.get("to");
-		System.out.println(myHashMap);
+		System.out.println(myLinkedHashmap);
+		int frequency = myLinkedHashmap.get("paranoid");
+		System.out.println("Paranoid frequency: "+frequency);
 	}
 
 }
